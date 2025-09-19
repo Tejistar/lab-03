c869462606cc77a98682ddc4446b723894a2fc97
Tejwinder Johal <tejwinderjohal@Tejwinders-MacBook-Air-2.local>
2025-09-19 13:23:02 -0600

    lab3_final

--- code/app/src/main/java/com/example/listycitylab3/MainActivity.java
+++ code/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -1,36 +1,86 @@
 package com.example.listycitylab3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        AddCityFragment.AddCityDialogListener, EditCityFragment.EditCityDialogListener{
 
-    private ArrayList<String> dataList;
+    public EditCityFragment editCity = new EditCityFragment();
+    private ArrayList<City> dataList;
     private ListView cityList;
-    private ArrayAdapter<String> cityAdapter;
+    private ArrayAdapter cityAdapter;
+    private Object last_city;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] cities = {
-                "Edmonton", "Vancouver", "Moscow",
-                "Sydney", "Berlin", "Vienna",
-                "Tokyo", "Beijing", "Osaka", "New Delhi"
-        };
+        String[] cities = { "Edmonton", "Vancouver", "Toronto" };
+        String[] provinces = { "AB", "BC", "ON" };
 
         dataList = new ArrayList<>();
-        dataList.addAll(Arrays.asList(cities));
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
         
         cityList = findViewById(R.id.city_list);
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityAdapter = new CityArrayAdapter(this, dataList);
         cityList.setAdapter(cityAdapter);
+
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AddCityFragment().show(getSupportFragmentManager(), "Add City");
+            }
+        });
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                City last_city = (City) parent.getItemAtPosition(position);
+                //last_city_string = last_city.toString();
+                EditCityFragment fragment = editCity.newInstance(last_city);
+                fragment.show(getSupportFragmentManager(), "Edit City");
+            }
+        });
     }
+    @Override
+    public void addCity(City city) {
+        cityAdapter.add(city);
+        cityAdapter.notifyDataSetChanged();
+    }
+
+    public void editCity(City city, City oldcity){
+        int position = 0;
+        for (int i = 0; i < cityAdapter.getCount(); i++){
+            City newCity = (City) cityAdapter.getItem(i);
+            if (newCity.getName() == oldcity.getName()){
+                position = i;
+            }
+        }
+        cityAdapter.remove(cityAdapter.getItem(position));
+        cityAdapter.insert(city, position);
+        cityAdapter.notifyDataSetChanged();
+    }
+
+    public Object getLast_city() {
+        return last_city;
+    }
+
+    //public String getLast_city_string() {
+    //    return last_city_string;
+    //}
 }
